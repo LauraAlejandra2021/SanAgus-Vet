f60007578dd09a15d78a8fb50136ce59fb09cfea
Camilo Agudelo <jcagudelo12@gmail.com>
2023-08-13 16:24:43 -0500

    Config.php como function, y pages-login formateado

--- vet/assets/db/config.php
+++ vet/assets/db/config.php
@@ -7,13 +7,15 @@ define('dbuser', 'root');
 define('dbpass', '');
 define('dbname', 'vetdog');
 
-// Connecting database
-try {
-	$connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
-	$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Function to establish database connection
+function connectDB() {
+    try {
+        $connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $connect;
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+        return null;
+    }
 }
-catch(PDOException $e) {
-	echo $e->getMessage();
-}
-
 ?>
