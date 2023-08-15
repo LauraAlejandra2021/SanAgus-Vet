b4f5f5d1dad4c614a77907e6e03c9858762762c3
Camilo Agudelo <jcagudelo12@gmail.com>
2023-08-15 15:40:42 -0500

    cambio en rutas del menu

--- vista/menu.php
+++ vista/menu.php
@@ -4,7 +4,7 @@
         <!-- User Info -->
         <div class="user-info">
             <div class="image">
-                <img src="../../assets/img/mujerico.png" width="48" height="48" alt="User" />
+                <img src="/vet/assets/img/mujerico.png" width="48" height="48" alt="User" />
             </div>
             <div class="info-container">
                 <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo ucfirst($_SESSION['nombre']); ?></div>
@@ -12,22 +12,19 @@
                 <div class="btn-group user-helper-dropdown">
                     <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                     <ul class="dropdown-menu pull-right">
-                        <li><a href="../config/configuracion"><i class="material-icons">brightness_low</i>Mi Cuenta</a></li>
+                        <li><a href="/vet/vista/config/configuracion"><i class="material-icons">brightness_low</i>Mi Cuenta</a></li>
                         <li role="separator" class="divider"></li>
                         <li role="separator" class="divider"></li>
-                        <li><a href="../pages-logout"><i class="material-icons">input</i>Cerrar Sesión</a></li>
+                        <li><a href="/vet/vista/pages-logout"><i class="material-icons">input</i>Cerrar Sesión</a></li>
                     </ul>
                 </div>
             </div>
         </div>
-        <a href="'/../panel-admin/administrador'; ?>">Enlace a Administrador</a>
-        <a href="'/../panel-admin/administrador'; ?>">Enlace a Administrador</a>
-
         <div class="menu">
             <ul class="list">
                 <li class="header">MENÚ DE NAVEGACIÓN</li>
                 <li class="active">
-                    <a href="/panel-admin/administrador">
+                    <a href="/vet/vista/panel-admin/administrador">
                         <i class="material-icons">home</i>
                         <span>INICIO</span>
                     </a>
@@ -43,7 +40,7 @@
                             <a href="/vet/vista/productos/nuevo">Registrar</a>
                         </li>
                         <li>
-                        <a href="../folder/productos">Listar / Modificar</a>
+                        <a href="/vet/folder/productos">Listar / Modificar</a>
                         </li>
                     </ul>
                 </li>
@@ -58,7 +55,7 @@
                             <a href="/vet/vista/categorias/nuevo">Registrar</a>
                         </li>
                         <li>
-                            <a href="../folder/categorias">Listar / Modificar</a>
+                            <a href="/vet/folder/categorias">Listar / Modificar</a>
                         </li>
                     </ul>
                 </li>
@@ -70,10 +67,10 @@
                     </a>
                     <ul class="ml-menu">
                         <li>
-                            <a href="/vista./vet/vista/clientes/nuevo">Registrar</a>
+                            <a href="/vet/vista/clientes/nuevo">Registrar</a>
                         </li>
                         <li>
-                            <a href="../folder/clientes">Listar / Modificar</a>
+                            <a href="/vet/folder/clientes">Listar / Modificar</a>
                         </li>
                     </ul>
                 </li>
@@ -88,7 +85,7 @@
                             <a href="/vet/vista/proveedores/nuevo">Registrar</a>
                         </li>
                         <li>
-                            <a href="../folder/proveedores">Listar / Modificar</a>
+                            <a href="/vet/folder/proveedores">Listar / Modificar</a>
                         </li>
                     </ul>
                 </li>
@@ -103,7 +100,7 @@
                             <a href="/vet/vista/veterinarios/nuevo">Registrar</a>
                         </li>
                         <li>
-                            <a href="../folder/veterinarios">Listar / Modificar</a>
+                            <a href="/vet/folder/veterinarios">Listar / Modificar</a>
                         </li>
                     </ul>
                 </li>
@@ -118,13 +115,13 @@
                             <a href="/vet/vista/mascotas/nuevo">Registrar</a>
                         </li>
                         <li>
-                            <a href="../folder/mascotas">Listar / Modificar</a>
+                            <a href="/vet/folder/mascotas">Listar / Modificar</a>
                         </li>
                         <li>
-                            <a href="../folder/tipo">Tipos</a>
+                            <a href="/vet/folder/tipo">Tipos</a>
                         </li>
                         <li>
-                            <a href="../folder/raza">Razas</a>
+                            <a href="/vet/folder/raza">Razas</a>
                         </li>
                     </ul>
                 </li>
@@ -139,11 +136,11 @@
                             <a href="/vet/vista/citas/nuevo">Registrar</a>
                         </li>
                         <li>
-                            <a href="../folder/citas">Listar / Modificar</a>
+                            <a href="/vet/folder/citas">Listar / Modificar</a>
                         </li>
 
                         <li>
-                            <a href="../folder/servicio">Servicio</a>
+                            <a href="/vet/folder/servicio">Servicio</a>
                         </li>
                     </ul>
                 </li>
@@ -158,11 +155,11 @@
                             <a href="/vet/vista/compra/nuevo">Registrar</a>
                         </li>
                         <li>
-                            <a href="../folder/compra">Listar / Modificar</a>
+                            <a href="/vet/folder/compra">Listar / Modificar</a>
                         </li>
 
                         <li>
-                            <a href="/compra/compras_fecha">Consultar por fecha</a>
+                            <a href="/vet/vista/compra/compras_fecha">Consultar por fecha</a>
                         </li>
                     </ul>
                 </li>
@@ -177,11 +174,11 @@
                             <a href="/vet/vista/venta/nuevo">Registrar</a>
                         </li>
                         <li>
-                            <a href="../folder/venta">Listar / Modificar</a>
+                            <a href="/vet/folder/venta">Listar / Modificar</a>
                         </li>
 
                         <li>
-                            <a href="/venta/venta_fecha">Consultar por fecha</a>
+                            <a href="/vet/vista/venta/venta_fecha">Consultar por fecha</a>
                         </li>
                     </ul>
                 </li>
@@ -196,7 +193,7 @@
                             <a href="/vet/vista/veterinarios/nuevo">Registrar</a>
                         </li>
                         <li>
-                            <a href="../folder/veterinarios">Listar / Modificar</a>
+                            <a href="/vet/folder/veterinarios">Listar / Modificar</a>
                         </li>
                     </ul>
                 </li>               
