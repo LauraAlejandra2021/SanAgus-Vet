0f78fc3b04fade3c118864d74b8d4a9c27530e47
mayii527 <mayicadavid.527@gmail.com>
2023-09-01 16:09:50 -0500

    US-077

--- modelo/modelopet.php
+++ modelo/modelopet.php
@@ -9,7 +9,7 @@ class Modelo{
       $this->db=new PDO('mysql:host=localhost;dbname=vetdog',"root","");
   }
   public function mostrar($tabla,$condicion){
-      $consulta="SELECT pet.id_pet, pet.nomas, pet_type.id_tiM, pet_type.noTiM, raza.id_raza, raza.nomraza, pet.sexo, pet.edad, pet.tamano, pet.peso, owner.id_due, owner.dni_due, owner.nom_due, owner.ape_due, owner.fecnaci, owner.movil, owner.fijo, owner.correo, owner.direc, pet.obser, pet.estado, pet.fere FROM pet INNER JOIN pet_type ON pet.id_tiM =  pet_type.id_tiM INNER JOIN raza ON pet.id_raza =raza.id_raza INNER JOIN owner ON pet.id_due = owner.id_due";
+      $consulta="SELECT pet.id_pet, pet.nomas, pet_type.id_tiM, pet_type.noTiM, raza.id_raza, raza.nomraza, pet.sexo, pet.edad, pet.tamano, pet.peso, owner.id_due, owner.dni_due, owner.nom_due, owner.ape_due, owner.movil, owner.fijo, owner.correo, owner.direc, pet.obser, pet.estado, pet.fere FROM pet INNER JOIN pet_type ON pet.id_tiM =  pet_type.id_tiM INNER JOIN raza ON pet.id_raza =raza.id_raza INNER JOIN owner ON pet.id_due = owner.id_due";
 
       $resultado=$this->db->query($consulta);
       while ($tabla=$resultado->fetchAll(PDO::FETCH_ASSOC)) {
