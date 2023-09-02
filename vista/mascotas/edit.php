ff7319acafbe2a49aa8e171a9a4c34caec1e41ff
Mayii Cadavid Aguilar <85380406+mayii527@users.noreply.github.com>
2023-09-02 12:29:05 -0500

    Develop (#80)

--- vista/mascotas/edit.php
+++ vista/mascotas/edit.php
@@ -110,7 +110,7 @@ if (!isset($_SESSION['cargo']) == 1) {
                             $db = new Database();
                             $con = $db->getMysqli();
                             $id = $_GET['id'];
-                            $sql = "SELECT pet.id_pet, pet.nomas, pet_type.id_tiM, pet_type.noTiM, raza.id_raza, raza.nomraza, pet.sexo, pet.edad, pet.tamano, pet.peso, owner.id_due, owner.dni_due, owner.nom_due, owner.ape_due, owner.fecnaci, owner.movil, owner.fijo, owner.correo, owner.direc, pet.obser, pet.estado, pet.fere FROM pet INNER JOIN pet_type ON pet.id_tiM =  pet_type.id_tiM INNER JOIN raza ON pet.id_raza =raza.id_raza INNER JOIN owner ON pet.id_due = owner.id_due  WHERE id_pet= '$id'";
+                            $sql = "SELECT pet.id_pet, pet.nomas, pet_type.id_tiM, pet_type.noTiM, raza.id_raza, raza.nomraza, pet.sexo, pet.edad, pet.tamano, pet.peso, owner.id_due, owner.dni_due, owner.nom_due, owner.ape_due, owner.movil, owner.fijo, owner.correo, owner.direc, pet.obser, pet.estado, pet.fere FROM pet INNER JOIN pet_type ON pet.id_tiM =  pet_type.id_tiM INNER JOIN raza ON pet.id_raza =raza.id_raza INNER JOIN owner ON pet.id_due = owner.id_due  WHERE id_pet= '$id'";
                             $query  = $con->query($sql);
                             $data =  array();
                             if ($query) {
