d0217aaca5eb3e262fe6a1d79ae4ab9a78cb9dbf
Juan Camilo Agudelo <jcagudelo12@gmail.com>
2023-09-05 12:15:46 -0500

    Develop (#95)
    Close #5
    * Cambio del metodo de cifrado de MD5 a SHA-256 (#42)
    
    * Conexion Database (#45)
    
    * US-014 (#44)
    
    * Conexion DataBase
    
    * Conexion DataBase
    
    * fix Conection
    
    * fix (#46)
    
    * US-015
    
    * conexion Database file:nuevo.php
    
    * fix
    
    * Conexion Database
    
    * Edit Vista Clientes
    
    * US-048: Reestructurar tabla Owner y sus respectivas vistas. (#51)
    
    * Delete campos
    
    * fix
    
    * Update Base de datos
    
    * Se quitan imagenes
    
    * Se quitan imagenes
    
    * Conexion Databases (#55)
    
    * Conexion Databases (#57)
    
    * Us 003 (#60)
    
    * Cambio del metodo de cifrado de MD5 a SHA-256
    
    * Cambio en validacion de contrasena
    
    ---------
    
    Co-authored-by: mayii527 <mayicadavid.527@gmail.com>
    
    * US-020: Reemplazar las conexiones de la db duplicadas en las vistas de Productos. (#68)
    
    * Conexion Databases
    
    * opcion Add Image
    
    * Se quita ; duplicada
    
    * US-016 (#71)
    
    * US-018: Reemplazar las conexiones de la db duplicadas en las vistas de Mascotas. (#74)
    
    * US-018
    
    * US-018
    
    * US-018
    
    * US-023 (#76)
    
    * US-021 (#75)
    
    * US-077: Corrección de consulta SQL para listar Mascotas (#79)
    
    * US-077
    
    * US-077
    
    * US-022 (#82)
    
    * US-017 (#81)
    
    * US-024: Reemplazar las conexiones de la db duplicadas en las vistas de Tipo. (#83)
    
    * Se deja el header separado
    
    * Se deja el loading separado (#92)
    
    * Se deja el loading separado (#94)
    
    ---------
    
    Co-authored-by: Mayii Cadavid Aguilar <85380406+mayii527@users.noreply.github.com>
    Co-authored-by: mayii527 <mayicadavid.527@gmail.com>
    Co-authored-by: LauraAlejandra2021 <85528940+LauraAlejandra2021@users.noreply.github.com>

--- vista/citas/cita_rapida.php
+++ vista/citas/cita_rapida.php
@@ -116,27 +116,8 @@ values ('$id_vet','$id_tiM','$id_servi','$title','$nommas','$dueno','$color','$s
 </head>
 
 <body class="theme-red">
-    <!-- Page Loader -->
-    <div class="page-loader-wrapper">
-        <div class="loader">
-            <div class="preloader">
-                <div class="spinner-layer pl-red">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div>
-                    <div class="circle-clipper right">
-                        <div class="circle"></div>
-                    </div>
-                </div>
-            </div>
-            <p>Cargando...</p>
-        </div>
-    </div>
-    <!-- #END# Page Loader -->
-
-    <!-- Overlay For Sidebars -->
-    <div class="overlay"></div>
-    <!-- #END# Overlay For Sidebars -->
+    <!-- Loading -->
+    <?php include_once __DIR__ . '../../commons/loading.php'; ?>
 
     <!-- Header -->
     <?php include_once __DIR__ . '../../commons/header.php'; ?>
