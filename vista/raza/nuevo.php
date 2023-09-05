cb1b11679479f928d6892e1e9e060acc32e83a24
Juan Camilo Agudelo <jcagudelo12@gmail.com>
2023-09-05 12:07:14 -0500

    Se deja el loading separado (#92)

--- vista/raza/nuevo.php
+++ vista/raza/nuevo.php
@@ -33,27 +33,8 @@ if (!isset($_SESSION['cargo']) == 1) {
 </head>
 
 <body class="theme-red">
-    <!-- Page Loader -->
-    <div class="page-loader-wrapper">
-        <div class="loader">
-            <div class="preloader">
-                <div class="spinner-layer pl-red">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div>
-                    <div class="circle-clipper right">
-                        <div class="circle"></div>
-                    </div>
-                </div>
-            </div>
-            <p>Cargando...</p>
-        </div>
-    </div>
-    <!-- #END# Page Loader -->
-
-    <!-- Overlay For Sidebars -->
-    <div class="overlay"></div>
-    <!-- #END# Overlay For Sidebars -->
+    <!-- Loading -->
+    <?php include_once __DIR__ . '../../commons/loading.php'; ?>
 
     <!-- Header -->
     <?php include_once __DIR__ . '../../commons/header.php'; ?>
